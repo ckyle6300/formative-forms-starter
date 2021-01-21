8c8ab9c95763bcb58ce1623ec53ffd3b1f784b2f
Christopher Kyle <ckyle6300@outlook.com>
2021-01-21 17:34:52 -0600

    3rd handoff

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const csurf = require('csurf');
+// const csurf = require('csurf');
+// const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
+app.use(express.urlencoded({ extended: true }));
 
+// const csrfProtection = csurf({ cookie: true });
 
 const users = [
    {
@@ -22,12 +25,18 @@ const users = [
 ];
 
 app.get("/", (req, res) => {
-   console.log({users})
-   res.render('index', {users})
+   console.log({ users })
+   res.render('index', { users })
 });
 
 app.get("/create", (req, res) => {
    res.render('create')
+})
+
+app.post("/create", (req, res) => {
+   console.log(req.body);
+   users.push(req.body)
+   res.redirect("/");
 })
 
 
