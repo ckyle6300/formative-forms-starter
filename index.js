831b707fb9ca498673e67546f890d6c190026e36
Matt Johnston <emollientspy1@gmail.com>
2021-01-21 14:51:38 -0800

    vyrm v1.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const csurf = require('csurf');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -12,12 +12,23 @@ const users = [
       firstName: "Jill",
       lastName: "Jack",
       email: "jill.jack@gmail.com"
-   }
+   },
+   {
+      id: 1,
+      firstName: "Jill2",
+      lastName: "Jack2",
+      email: "jill.jack@gmail.com2"
+   },
 ];
 
 app.get("/", (req, res) => {
-   res.render('index', users)
+   console.log({users})
+   res.render('index', {users})
 });
+
+app.get("/create", (req, res) => {
+   res.render('create')
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
